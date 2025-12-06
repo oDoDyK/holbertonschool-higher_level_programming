4bfac437be4f16e2a7c2c314cd23fd72efc9b255
oDoDyK <4thkiller2005@gmail.com>
2025-12-06 01:47:50 +0000

    feat: implement safe_print_integer for printing and counting integers

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,17 @@
+#!/usr/bin/python3
+
+
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
+
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-            printed += 1
-        except (ValueError, TypeError):
+            if type(my_list[i]) is int and printed != x:
+                print('{:d}'.format(my_list[i]), end='')
+                printed += 1
+        except TypeError:
             continue
-        except IndexError:
-            break
+
     print()
+
     return printed
