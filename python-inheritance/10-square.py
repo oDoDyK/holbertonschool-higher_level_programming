86c2855505f60fa127041816524cd2134cbe6469
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2025-12-22 20:07:16 +0300

    Add Square class inheriting from Rectangle
    Created a Square class that inherits from Rectangle, with size validation.

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Instantiation with size
+    """
+
+    def __init__(self, size):
+        """
+        size must be private. No getter or setter
+        size must be a positive integer,
+        validated by integer_validator
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
