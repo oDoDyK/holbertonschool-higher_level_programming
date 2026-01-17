f3750f68e824731a62b7912667f1aee83ece4e39
oDoDyK <4thkiller2005@gmail.com>
2026-01-17 17:12:34 +0000

    update

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Lists all states from the database hbtn_0e_0_usa filtered by name.
-Usage: ./1-filter_states.py <mysql username> <mysql password> <database name> <state name>
+Usage: ./2-my_filter_states.py <mysql username> <mysql password>
+<database name> <state name>
 """
 
 import sys
@@ -9,6 +10,7 @@ import MySQLdb
 
 
 def list_all_states():
+    """Lists all states filtered by name using format."""
     if len(sys.argv) != 5:
         sys.exit(1)
 
@@ -17,29 +19,30 @@ def list_all_states():
     database_name = sys.argv[3]
     match = sys.argv[4]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        user=user,
-        passwd=password,
-        db=database_name,
-        port=3306
-    )
-
-    cur = db.cursor()
-
-    query = (
-        "SELECT * FROM states "
-        "WHERE name LIKE BINARY '{}' "
-        "ORDER BY id ASC"
-    ).format(match)
-
-    cur.execute(query)
-
-    for row in cur.fetchall():
-        print(row)
-
-    cur.close()
-    db.close()
+    db = None
+    cur = None
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=user,
+            passwd=password,
+            db=database_name,
+            port=3306
+        )
+        cur = db.cursor()
+        query = ("SELECT * FROM states "
+                 "WHERE name LIKE BINARY '{}' "
+                 "ORDER BY id ASC").format(match)
+        cur.execute(query)
+        for state in cur.fetchall():
+            print(state)
+    except MySQLdb.Error as e:
+        print(f"Error connecting to database: {e}")
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
 
 
 if __name__ == "__main__":
