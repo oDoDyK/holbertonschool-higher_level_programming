482d3618ab2a48864be2e2a0f975f5eddd636402
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-01-17 19:52:20 +0300

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,34 +3,32 @@
 Lists all states from the database hbtn_0e_0_usa sorted by id.
 Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
 """
+
 import sys
 import MySQLdb
 
 
-def list_all_states():
-    """
-    Lists all states from the database hbtn_0e_0_usa sorted by id.
-    Usage: ./0-select_states.py <mysql username> <mysql password>
-    <database name>
-    """
-    try:
-        user1 = sys.argv[1]
-        pass1 = sys.argv[2]
-        db1 = sys.argv[3]
-        database = MySQLdb.connect(
-            port=3306, host="localhost", user=user1, passwd=pass1, db=db1
-        )
-        c = database.cursor()
-        c.execute("SELECT * FROM states ORDER BY states.id ASC")
-        [print(state) for state in c.fetchall()]
-    except MySQLdb.Error as e:
-        print(f"Error connecting to databse, {e}")
-    finally:
-        if c:
-            c.close()
-        if database:
-            database.close()
-
-
 if __name__ == "__main__":
-    list_all_states()
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=password,
+        db=database_name,
+        port=3306
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
