c64959b383888ce9ccea2e81a4f0394ae56fbea6
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-01-17 19:25:38 +0300

    Create database and cities table in MySQL
    This script creates the database 'hbtn_0d_usa' and the 'cities' table with a foreign key reference to the 'states' table.

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+
+-- create the `hbtn_0d_usa` database
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+--  select the `hbtn_0d_usa` database
+USE `hbtn_0d_usa`;
+
+-- create the `states` table
+CREATE TABLE IF NOT EXISTS `cities` (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+state_id INT NOT NULL, name VARCHAR(256) NOT NULL,
+FOREIGN KEY (state_id) REFERENCES states(id));
