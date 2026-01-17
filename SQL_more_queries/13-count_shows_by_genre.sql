a50b96709a265d67fa22df2bcba3d213199a8160
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-01-17 19:31:29 +0300

    Add query to count shows by genre
    This query lists all genres from the database and counts the number of shows linked to each genre, ordering the results by the number of shows in descending order.

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name AS 'genre', COUNT(tv_shows.title) AS 'number_of_shows'
+FROM tv_shows INNER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY genre
+ORDER BY number_of_shows DESC
