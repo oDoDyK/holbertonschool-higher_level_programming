a3e88612d100e27a0f0be8675f32f00971b6ab46
oDoDyK <4thkiller2005@gmail.com>
2026-01-17 17:21:41 +0000

    update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
 """
-Script that takes arguments and displays all cities of a given state
-from the database hbtn_0e_4_usa.
-Results are sorted by cities.id using SQLAlchemy.
+Module that defines the State class mapped to the 'states' table
+using SQLAlchemy ORM.
 """
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-from model_city import City
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        sys.exit(1)
+Base = declarative_base()  # Base class for all models
 
-    user = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+class State(Base):
+    """State class mapped to 'states' table"""
+    __tablename__ = 'states'
 
-    # Create engine
-    engine = create_engine(f'mysql+mysqldb://{user}:{password}@localhost/{db_name}')
-
-    # Bind session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query cities for the given state
-    result = session.query(City).join(State).filter(State.name == state_name)\
-        .order_by(City.id).all()
-
-    print(", ".join([city.name for city in result]))
-
-    session.close()
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
