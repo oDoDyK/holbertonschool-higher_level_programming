8195f019308479882149d0f2c182cab9402a9195
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-01-17 19:32:04 +0300

    Add query to list genres for 'Dexter'
    Lists all genres from hbtn_0d_tvshows and counts shows linked to each genre for 'Dexter'.

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name
+FROM tv_shows INNER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE title = 'Dexter'
+ORDER BY tv_genres.name
