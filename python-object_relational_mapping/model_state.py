c32e2beb14ec4bc1b2decf868e8a1ca29ab0a56d
oDoDyK <4thkiller2005@gmail.com>
2026-01-17 17:19:44 +0000

    update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,11 +1,45 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
 """
-import sys
-from model_state import Base, State
+Script that takes arguments and displays all cities of a given state
+from the database hbtn_0e_4_usa.
+Results are sorted by cities.id and safe from SQL injection.
+"""
 
-from sqlalchemy import (create_engine)
+import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=password,
+        db=database,
+        port=3306
+    )
+
+    cursor = db.cursor()
+
+    query = (
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
+    )
+
+    cursor.execute(query, (state_name,))
+    rows = cursor.fetchall()
+
+    print(", ".join([row[0] for row in rows]))
+
+    cursor.close()
+    db.close()
