d99817e56df5b0df36496dc7219bdd15f33e720e
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-01-17 19:50:29 +0300

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,36 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
+"""
+Lists all states from the database hbtn_0e_0_usa sorted by id.
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+"""
 import sys
+import MySQLdb
+
+
+def list_all_states():
+    """
+    Lists all states from the database hbtn_0e_0_usa sorted by id.
+    Usage: ./0-select_states.py <mysql username> <mysql password>
+    <database name>
+    """
+    try:
+        user1 = sys.argv[1]
+        pass1 = sys.argv[2]
+        db1 = sys.argv[3]
+        database = MySQLdb.connect(
+            port=3306, host="localhost", user=user1, passwd=pass1, db=db1
+        )
+        c = database.cursor()
+        c.execute("SELECT * FROM states ORDER BY states.id ASC")
+        [print(state) for state in c.fetchall()]
+    except MySQLdb.Error as e:
+        print(f"Error connecting to databse, {e}")
+    finally:
+        if c:
+            c.close()
+        if database:
+            database.close()
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    list_all_states()
