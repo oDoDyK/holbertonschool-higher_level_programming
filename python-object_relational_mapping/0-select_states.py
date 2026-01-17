95fb1b4cc0af9a57e944fe94ea23ac4df5aa975c
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-01-17 19:47:52 +0300

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,33 +1,16 @@
-# this module contains SQL scripts that lists all states from a database which start with "N"
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    from sys import argv
-    import MySQLdb
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        port=3306
-    )
-
-
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
-
-    # Execute the SQL query to retrieve states
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-
-    # Fetch all the rows
-    states = cursor.fetchall()
-
-    # Display the results
-    for state in states:
-        print(state)
-
-
-    # Close the cursor and connection
-    cursor.close()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
