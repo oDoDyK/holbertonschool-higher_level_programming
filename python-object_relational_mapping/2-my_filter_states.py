37532e4d3cc01257d2356bb5e898112329277b22
oDoDyK <4thkiller2005@gmail.com>
2026-01-17 17:11:11 +0000

    update

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,40 +1,45 @@
 #!/usr/bin/python3
 """
-Lists all states from the database hbtn_0e_0_usa sorted by id.
-Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+Lists all states from the database hbtn_0e_0_usa filtered by name.
+Usage: ./1-filter_states.py <mysql username> <mysql password> <database name> <state name>
 """
+
 import sys
 import MySQLdb
 
 
 def list_all_states():
-    """
-    Lists all states from the database hbtn_0e_0_usa sorted by id.
-    Usage: ./0-select_states.py <mysql username> <mysql password>
-    <database name>
-    """
-    try:
-        user1 = sys.argv[1]
-        pass1 = sys.argv[2]
-        db1 = sys.argv[3]
-        match = sys.argv[4]
-        database = None
-        c = None
-        database = MySQLdb.connect(
-            port=3306, host="localhost", user=user1, passwd=pass1, db=db1
-        )
-        c = database.cursor()
-        query = "SELECT * FROM states WHERE name LIKE BINARY %s\
-                ORDER BY states.id ASC"
-        c.execute(query,(match,))
-        [print(state) for state in c.fetchall()]
-    except MySQLdb.Error as e:
-        print(f"Error connecting to databse, {e}")
-    finally:
-        if c:
-            c.close()
-        if database:
-            database.close()
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    match = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=password,
+        db=database_name,
+        port=3306
+    )
+
+    cur = db.cursor()
+
+    query = (
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY '{}' "
+        "ORDER BY id ASC"
+    ).format(match)
+
+    cur.execute(query)
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
